2940586c004dbbdd6a5409186c9d72768d7d9971
Billy Wong <bwong.yh@protonmail.com>
2023-02-19 12:50:23 +0900

    create new user with faker

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,14 @@
-class User {}
+import { faker } from '@faker-js/faker';
+
+class User {
+  constructor(
+    public name: string,
+    public location: { lat: number; long: number }
+  ) {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      long: parseFloat(faker.address.longitude()),
+    };
+  }
+}
